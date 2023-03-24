ec152aa4c02958e70df336add6fddfb1d06c550d
Sandeep Kumar <sandeepqmar05@gmail.com>
2023-03-25 00:15:51 +0530

    Added basic loading animations

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
 const cards = Array.from(document.getElementsByClassName("card"));
 
-function handleCardLeave() {
-	Array.from(this.children).filter((child) => child.classList.contains("card-list")).map((list) => {list.style.display="none"});
-}
 function handleCardEnter() {
-	Array.from(this.children).filter((child) => child.classList.contains("card-list")).map((list) => {list.style.display="block"});
-	
+  Array.from(this.children).filter((child) =>
+    child.classList.contains("card-list")
+  ).map((list) => {
+    list.style.visibility = "visible";
+    //list.style.display = "block";
+    list.style.width = "auto";
+    list.style.height = "auto";
+    list.style.opacity = "1";
+    list.style.filter = "blur(0px)";
+  });
+}
+function handleCardLeave() {
+  Array.from(this.children).filter((child) =>
+    child.classList.contains("card-list")
+  ).map((list) => {
+    list.style.visibility = "hidden";
+ //   list.style.display = "none";
+    list.style.width = 0;
+    list.style.height = 0;
+    list.style.opacity = 0;
+    list.style.filter = "blur(50px)";
+  });
 }
 
 cards.map((card, i) => {
@@ -27,4 +44,12 @@ cards.map((card, i) => {
 cards.map((card) => {
   card.addEventListener("mouseenter", handleCardEnter);
   card.addEventListener("mouseleave", handleCardLeave);
+});
+["#sidebar", "#logo", ".sidebar-children", "#content", "#slideshow", ".card"].forEach((e, index) => {
+	setTimeout(
+	() => {document.querySelectorAll(e).forEach((i) => {
+		i.style.opacity = "1";
+		i.style.filter = "blur(0px)";
+		i.style.transform = "translateY(0rem)";
+	})}, index*400);	
 });
